92b9a22afcd8fc015a97c5c401ea131a248d02e5
TrineBuxbom <54663501+TrineBuxbom@users.noreply.github.com>
2019-09-30 11:27:46 +0200

    Animation til collapsible

--- js-slide.js
+++ js-slide.js
@@ -45,3 +45,19 @@ for(let i=0; i < slides.length; i++){
 
 // Til sidst når alt er loaded, kører vi funktionen.
 showSlides();
+
+// TRINES COLLAPSIBLES
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeList");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
